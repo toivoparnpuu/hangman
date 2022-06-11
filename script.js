9f0d747dec36f8bd66ce947c41007f1a9d8948a9
Toivo Pärnpuu <toivo.parnpuu@tlu.ee>
2022-06-11 13:07:55 +0300

    final version

--- script.js
+++ script.js
@@ -1,43 +1,73 @@
 const selectors = {
-    resetButton: '.js-reset-btn',
     rollD20: '.js-d20-btn',
     rollD90: '.js-d90-btn',
     charButton: '.js-char-btn',
     input: '.js-char-input',
     wordOutput: '.js-word',
+    livesOutput: '.js-lives',
     writeout: '.js-list'
 }
-const wordlist = ['kalamees', 'kartul', 'keevitaja', 'ventilaator', 'arvutimäng', 'ristsõna', 'aabits', 'karburaator', 'juhtplokk', 'raudbetoon', 'ilmajaam', 'kanala'];
+const wordlist = ['kalamees',
+    'kartul',
+    'keevitaja',
+    'ventilaator',
+    'arvutimäng',
+    'ristsõna',
+    'aabits',
+    'karburaator',
+    'juhtplokk',
+    'raudbetoon',
+    'ilmajaam',
+    'kanala'
+];
 let = orgWord = [];
-let = word = [];
+let = word = ['_'];
 let = d20result = 0;
 let = dxresult = 0;
-const resetButton = document.querySelector(selectors.resetButton);
+let = lives = 6;
 const charButton = document.querySelector(selectors.charButton);
 const rollD20 = document.querySelector(selectors.rollD20);
 const rollD90 = document.querySelector(selectors.rollD90);
 const input = document.querySelector(selectors.input);
 const writeout = document.querySelector(selectors.writeout);
 const wordOutput = document.querySelector(selectors.wordOutput);
+const livesOutput = document.querySelector(selectors.livesOutput);
 
 //event listeners
-resetButton.addEventListener("click", function () {
-    resetInputAndLog();
+charButton.addEventListener("click", function () {
+    checkChar(input.value[0].toLowerCase());
+    displayLives();
+    displayWord();
 });
-
 rollD20.addEventListener("click", function () {
     clickOnRollDice(20);
     rollD20.disabled = true;
+    rollD90.disabled = false;
+    if (d20result == 20) {
+        lives = lives + 1;
+    } else if (d20result == 1) {
+        lives = 0;
+    }
+    displayLives();
 });
 
 rollD90.addEventListener("click", function () {
     clickOnRollDice(10);
     rollD90.disabled = true;
+    charButton.disabled = false;
     prepWord();
+    displayWord();
+    displayLives();
 });
 
+
 window.addEventListener("load", function () {
+    rollD20.disabled = false;
+    rollD90.disabled = true;
+    charButton.disabled = true;
     choosWord();
+    displayLives();
+    displayWord();
 });
 
 // functions
@@ -50,31 +80,52 @@ function clickOnRollDice(sides) {
         displayResults(`D20 tulemus: ${d20result}`);
     }
 }
-function choosWord(){
+function checkChar(userChar) {
+    if (orgWord.indexOf(userChar) > -1) {
+        orgWord.forEach((element, i) => {
+            if (element == userChar) {
+                word[i] = userChar;
+            }
+        }); // foreach
+    } else {
+        lives = lives - 1;
+    }
+}
+function choosWord() {
     orgWord = wordlist[Math.floor(Math.random() * wordlist.length)].split('');
     displayResults('debug: arvatav sõna: ' + orgWord.join(''));
-
 }
-function prepWord(){
-    let openSymbols = Math.floor(orgWord.length * ((dxresult /100) / 2));
+
+function prepWord() {
+    let openSymbols = Math.floor(orgWord.length * ((dxresult / 100) / 2));
     displayResults('avatavad sümbolid on ' + openSymbols);
     orgWord.forEach(element => {
         if (openSymbols > 0 && Boolean(Math.round(Math.random()))) {
-            openSymbols = openSymbols -1;
+            openSymbols = openSymbols - 1;
             word.push(element);
         } else {
             word.push('_');
-            console.log('loeme sõnu');    
-        }   
+            console.log('loeme sõnu');
+        }
     });
-
-    displayWord();
 }
-function resetInputAndLog() {
-    input.value = "";
-    while (writeout.firstChild) {
-        writeout.removeChild(writeout.firstChild);
+function displayLives() {
+    if (lives < 1) {
+        rollD20.disabled = true;
+        rollD90.disabled = true;
+        charButton.disable = true;
+        livesOutput.innerHTML = lives;
+        window.alert('Mäng läbi!');
+
+    } else {
+        livesOutput.innerHTML = lives;
     }
+    //check win
+    if(word.indexOf('_') == -1){
+        window.alert('Oled võitnud! uuesti mängimiseks tee refresh');
+        charButton.disable = true;
+    }
+
 }
 
 function displayResults(textRow) {
@@ -83,6 +134,7 @@ function displayResults(textRow) {
     writeout.appendChild(li);
 }
 
-function displayWord(){
+function displayWord() {
+    console.log(word.join(''));
     wordOutput.innerHTML = word.join('');
 }
